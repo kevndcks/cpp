ccaa700d536563d93fd5ffa83b4e1373c32107fb
Kevin Dicks <kevndcks@aol.com>
2018-04-23 00:07:43 +0100

    Hello World

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> /* include iostream header */
+
+using namespace std; /* namespace scope of definitions */
+
+int main() /* main function */
+{
+    cout << "Hello world!" << endl; /* standard output stream */
+    return 0; /* return integer value for main */
+}
